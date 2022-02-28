c27fe23a2513a1b52b80264c25794fe04d97d5ef
amanjr09-glitch <83840673+amanjr09-glitch@users.noreply.github.com>
2022-02-28 22:41:37 +0530

    changes in footer, signup

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../sass/components/Profile.css';
 import { authentication } from '../firebase/firebase';
 import { useHistory } from "react-router-dom";
+
+
 function Profile(){
     const history = useHistory();
     const doLogout = () => {
@@ -15,7 +17,6 @@ function Profile(){
     <div class="member">
         <p id="name"><b>Madhu Sudan Singh</b></p>
         <p id="mail">singhmadhusudan1998@gmail.com</p>
-         <a href="">Get Exclusive Membership</a>
     </div>
     <div class="sidenav">
     
@@ -25,23 +26,18 @@ function Profile(){
     
     <ul>
         <li>Gift Vouchers</li>
-        <li>Tss Points</li>
-        <li>Tss Monry</li>
-        <li>Saved Cards</li>
         <li>FAQs</li>
         <li>Profile</li>
     </ul>
     
-    <ul>
-        <li>Submit Designs</li>
-    </ul>
+   
     
-    <button onClick={doLogout}>logout</button>
+    <button onClick={doLogout}>LOGOUT</button>
     </div>
     </div>
     
     <div class="container2">
-        <h6>Edit Profile</h6>
+        <h4> <b>Edit Profile</b> </h4>
     
         <div class="email">
             <form action=""/>
@@ -50,18 +46,18 @@ function Profile(){
               
         </div>
 
+        <h5>General Information</h5>
+
     <div class="detail">
-        <h6>General Information</h6>
-        <hr/>
         <div class="forme">
 
             <div class="form1">
                 <form action=""/>
                     <label for="fname">First Name*</label><br/>
-                  <input type="text" id="Firstname" name="Firstname" value="" required/><br/>
+                  <input type="text" id="Firstname" name="Firstname" required/><br/>
                 <form action=""/>
                     <label for="lname">Last Name</label><br/>
-                  <input type="text" id="Lastname" name="Lastname" value="" /><br/>
+                  <input type="text" id="Lastname" name="Lastname"  /><br/>
                 <div class="radio">
                     <label for="lname">Gender</label><br/>
                     <form action=""/>
@@ -77,16 +73,16 @@ function Profile(){
             <div class="form2">
                 <form action=""/>
                     <label for="fname">Date Of Birth</label> <br/>
-                  <input type="date" id="dob" name="dob" value=""/ ><br/>
+                  <input type="date" id="dob" name="dob" / ><br/>
                 <form action=""/>
                     <label for="lname">Mobile No</label><br/>
-                  <input type="text" id="mobileno" name="mobileno" value="" required/><br/>
+                  <input type="text" id="mobileno" name="mobileno" required/><br/>
                 <form action=""/>
                     <label for="lname">Address</label><br/>
-                  <input type="textarea" id="address" name="address" value="" required/><br/>
+                  <input type="textarea" id="address" name="address" required/><br/>
          <hr/>
               <div class="button">
-                 <button>save</button>
+                 <button>SAVE</button>
               </div>
     </div>
     </div>
